ba19a7c091372250522527b1c3f89d3c67fade58
srjagankumar <srjagankumar@gmail.com>
2024-11-25 11:58:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,9 +14,7 @@ from snowflake.snowpark.functions import col
 name_on_order = st.text_input('Name on the Smoothie:')
 st.write('The name on your Smoothie order will be:',name_on_order)
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+
 
 cnx=st.connection("snowflake")
 session = cnx.session()
@@ -44,3 +42,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!'+name_on_order,icon="✅")
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
